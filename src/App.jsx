ddeebe221d4e76d26b942a2873fa89e5232dd732
Benjamin Shupe <benshupe@sasktel.net>
2025-07-23 13:50:20 -0600

    import fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
-import Home from './pages/home'
+import Home from './pages/Home'
 
 export default function App() {
   const [count, setCount] = useState(1)
