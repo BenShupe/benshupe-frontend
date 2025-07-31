5e7304962b9d028155deab64ba6d6d9c04d9356f
Benjamin Shupe <benshupe@sasktel.net>
2025-07-30 18:10:29 -0600

    update look

--- src/components/GlowButton.jsx
+++ src/components/GlowButton.jsx
@@ -3,7 +3,7 @@ import './GlowButton.css'
 export default function Button({color, onClick, children}) {
     return (
     <>
-        <button style={{"--color-button": color}} onClick={onClick}>
+        <button className="glow-button" style={{"--color-button": color}} onClick={onClick}>
             {children}
         </button>
     </>
