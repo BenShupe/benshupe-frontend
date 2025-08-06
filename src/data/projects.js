1810ef286ca00c8c1415c0dd5b55b4a38c3d7770
Benjamin Shupe <benshupe@sasktel.net>
2025-08-05 22:15:23 -0600

    update description

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
         title: "Web Portfolio",
-        description: "A modern, serverless web portfolio built with React, AWS S3, AWS SES for emails, Lambda for backend, and DB for message storage.",
+        description: "A modern web portfolio built using React, AWS (S3, SES, and Lambda), Cloudflare DNS, and DB for message storage.",
         image_path: "/AWS-Portfolio-Home.png",
         github: "https://github.com/BenShupe/benshupe-frontend",
         live: "https://benshupe.com",
@@ -10,7 +10,7 @@ export const projects = [
     },
     {
         title: "Flappy Bird AI",
-        description: "A self-learning AI that plays Flappy Bird using a genetic algorithm. Built from scratch with a simple neural network and trained over generations to improve performance without any labeled data.",
+        description: "A self-learning AI that plays Flappy Bird. Built from scratch with a simple neural network and trained over generations to improve.",
         image_path: "/Flappy-Bird-AI.png",
         github: "https://github.com/BenShupe/ai-flappy-bird",
         live: "#",
