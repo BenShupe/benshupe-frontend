77aa695cdd3e778bf4c0ef9770822fa9922b87aa
Benjamin Shupe <benshupe@sasktel.net>
2025-08-22 23:40:45 -0600

    add disabled functionality

--- src/components/GlowButton.jsx
+++ src/components/GlowButton.jsx
@@ -1,9 +1,9 @@
 import './GlowButton.css'
 
-export default function Button({color, onClick, children}) {
+export default function Button({color, onClick, disabled, children}) {
     return (
     <>
-        <button className="glow-button" style={{"--color-button": color}} onClick={onClick}>
+        <button className="glow-button" style={{"--color-button": color}} onClick={onClick} disabled={disabled}>
             {children}
         </button>
     </>
